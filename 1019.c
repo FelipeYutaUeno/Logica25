c1ef9d4e64da86712d2d05b2c0aa338c865a7043
FelipeYutaUeno <felipeyutaueno@gmail.com>
2025-05-14 18:13:45 -0300

    Create 1019.c

---
+++ 1019.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int h, m, s, N;
+    scanf("%d", &N);
+    h = N / 3600;
+    N %= 3600;
+    m = N / 60;
+    N %= 60;
+    s = N;
+    printf("%d:%d:%d\n", h, m, s);
+ 
+    return 0;
+}
